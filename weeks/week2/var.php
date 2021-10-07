0385c4867331dd43c285fcafeaad7a0475a88d56
efaunt <iwata.mich@gmail.com>
2021-10-06 20:59:04 -0700

    10.6_push

---
+++ weeks/week2/var.php
@@ -0,0 +1,34 @@
+<?php
+
+$name = 'Michito';
+$weather = 'Today is rainy';
+$body_temp = 55.7;
+
+echo $name;
+echo 'My name is $name';
+echo '<br>';
+echo "My name is $name";
+echo '<br>';
+echo 'My name is ' .$name.' ' ;
+echo '<br>';
+echo $weather;
+echo '<br>';
+echo $body_temp;
+echo '<br>';
+echo 'The normal body temperature is 
+'.$body_temp.' and is cold!'; 
+$name = 'not Michito';
+echo '<br>';
+echo $name;
+echo '<br>';
+$x = 20;
+$y = 5;
+echo $x;
+echo '<br>';
+$z = $x + $y;
+//if you use double quotes, variable value will display//
+
+
+
+
+6llgCcupb@rNL2KS*OXN

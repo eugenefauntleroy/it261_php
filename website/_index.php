1c070e1fd6cafa1ce5186471478dfcf0ca37153c
efaunt <iwata.mich@gmail.com>
2021-10-12 19:31:51 -0700

    olga_edits

--- website/_index.php
+++ website/_index.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0" >
-<link href="css/styles.css" type="text/css" rel="stylesheet">
+<link href="css/style.css" type="text/css" rel="stylesheet">
 <title>Home page of our website</title>
 
 </head>
@@ -16,7 +16,7 @@
         alt="LOGO">
         </a>
         <nav>
-    <ul>
+            <ul>
 <li><a href="">Home</a></li> 
 <li><a href="">About</a></li>
 <li><a href="">Daily<a></li>
@@ -27,38 +27,34 @@
     </nav>
     </div> <!--en d header inner-->
     </header>
-    <div id="wrapper">
-        <h2>Website</h2>
+<div id="wrapper">
+
+<main>
+        <h2>Web Site</h2>
         <p>About Me:</p>
         <p>I am a student at Seattle Central college focusing on data analytics and management. I plan to attend WSU next year to pursue a 
             degree in political science, focusing on international studies. Other than that, I enjoy traveling and music. 
         </p>
-        <img src="images/mich_dig copy.jpg" alt="This is my image" width="250" height="250"/>
-        <h3>Week 2</h3>
-        <ul>
-        <li><a href="/Applications/MAMP/htdocs/it261/weeks/week2/var.php">var.php</a></li> 
-        <li><a href="var2.php">var2.php</a></li>
-        <li><a href="currency-logic.php">currency-logic.php<a></li>
-        <li><a href="week2/currency.php">currency</a></li>
-        <li><a href="weeks/week2/heredoc.php">heredoc</a></li>
-
-     </ul>
+        <img src="images/mich_dig copy.jpg" alt="This is my image" width="240" height="240"/>
     <div id="hero">
-
+    <img src="images/ss_1.png" alt="This is my image" width="680" height ="250"/>
+    <img src="images/ss_2.png" alt="This is my image"width="680"/>
     </div> <!--end hero-->
-<main>
-
-<?php
-
-//print_r($vegetables);
-
-?>
-
 </main>
     
-<aside>
-        
-</aside>
+    <aside>
+            <h2>My Class Exercises</h2>
+            <h3>Week 2</h3>
+            <ul>
+            <li><a href="weeks/week2/var.php">var.php</a></li> 
+            <li><a href="weeks/week2/var2.php">var2.php</a></li>
+            <li><a href="weeks/week2/currency-logic.php">currency-logic.php<a></li>
+            <li><a href="weeks/week2/currency.php">currency</a></li>
+            <li><a href="weeks/week2/heredoc.php">heredoc</a></li> 
+            </ul>      
+    </aside>
+</div> 
+<!--end wrapper-->
 <footer>
 <ul>
 <li>Copyright &copy; 2021</li>
@@ -67,13 +63,8 @@
 <li><a href="">Web Design by Michito</a></li>
 <li><a href="">HTML Validation</a></li>
 <li><a href="">CSS Validation</a></li>
-
-
-
 </ul>
 </footer>
     
-    </div> <!--end wrapper-->
-
 </body>
 </html>
